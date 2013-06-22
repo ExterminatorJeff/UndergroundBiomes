3b506b5787ebe66bfb80c5375620d3681b31575a
Grom PE <i@grompe.org.ru>
2013-06-22 22:45:24 +0700

    Replaced flint with dolomite

--- src/exterminatorJeff/undergroundBiomes/common/block/BlockSedimentaryStone.java
+++ src/exterminatorJeff/undergroundBiomes/common/block/BlockSedimentaryStone.java
@@ -18,9 +18,9 @@ import exterminatorJeff.undergroundBiomes.common.item.ItemFossilPiece;
 
 public class BlockSedimentaryStone extends BlockMetadataBase
 {
-    private static final float[] hardness = {0.5f, 0.5f, 0.5f, 0.6f, 0.5f, 1.0f, 1.0f, 0.9f};
+    private static final float[] hardness = {0.5f, 0.5f, 0.5f, 0.6f, 0.5f, 0.5f, 1.0f, 0.9f};
     private static final String[] blockName = {
-        "limestone", "chalk", "shale", "siltstone", "ligniteBlock", "flint", "greywacke", "chert"
+        "limestone", "chalk", "shale", "siltstone", "ligniteBlock", "dolomite", "greywacke", "chert"
     };
 
     public BlockSedimentaryStone(int id)
@@ -48,13 +48,13 @@ public class BlockSedimentaryStone extends BlockMetadataBase
             {
                 return new ItemStack(Item.clay, 1, 0);
             }
-            // Limestone, chalk, siltstone and lignite drop fossil pieces
-            if (metadata == 0 || metadata == 1 || metadata == 3 || metadata == 4)
+            // Limestone, chalk, siltstone, lignite and dolomite drop fossil pieces
+            if (metadata == 0 || metadata == 1 || metadata == 3 || metadata == 4 || metadata == 5)
             {
                 return new ItemStack(UndergroundBiomes.fossilPiece.itemID, 1, random.nextInt(ItemFossilPiece.TYPES));
             }
-            // Chert and flint drop flint item
-            if (metadata == 5 || metadata == 7)
+            // Chert drops flint item
+            if (metadata == 7)
             {
                 return new ItemStack(Item.flint, 1, 0);
             }
